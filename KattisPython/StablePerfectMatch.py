407173bec11c027aa74bd56beb2d23a17844858e
Laurits <laku@itu.dk>
2024-08-29 23:11:47 +0200

    complete stable

--- KattisPython/StablePerfectMatch.py
+++ KattisPython/StablePerfectMatch.py
@@ -13,9 +13,6 @@ def read_input():
 
     n, m = map(int, data[0].split())
 
-    # Data will store names, stacks (preferences), and maps (rankings)
-
-
     # Reading data for each participant
     index = 1
     for _ in range(n):
@@ -59,7 +56,6 @@ def split_names():
     global proposers
     global rejectors
 
-    # Split the list into two parts
     proposers = names[:len(names) // 2]
     rejectors = names[len(names) // 2:]
 
@@ -73,16 +69,16 @@ def tinder():
     remaining_proposers = set(proposers)
     while remaining_proposers:
         proposer = remaining_proposers.pop()
-        print()
-        print(f"Current proposer: {proposer}.")
+        #print()
+        #print(f"Current proposer: {proposer}.")
         proposers_prefs = stacks[proposer]
 
         while proposers_prefs:
             rejector = proposers_prefs.pop()
-            print(f"Current rejector: {rejector}.")
+            #print(f"Current rejector: {rejector}.")
             if rejector not in matches:
-                print(f"Matched {rejector} with {proposer}.")
-                print()
+                #print(f"Matched {rejector} with {proposer}.")
+                #print()
                 matches[rejector] = proposer
                 matches[proposer] = rejector
                 break
@@ -91,43 +87,47 @@ def tinder():
                 existing_match = matches[rejector]
                 current_match_rank = rejectors_prefs[existing_match]
                 proposer_match_rank = rejectors_prefs[proposer]
-                print(f"{rejector} already matched with {existing_match} with rank {current_match_rank}.")
-                print(f"{proposer} has a rank of {proposer_match_rank}.")
-                print()
+                #print(f"{rejector} already matched with {existing_match} with rank {current_match_rank}.")
+                #print(f"{proposer} has a rank of {proposer_match_rank}.")
+                #print()
 
                 if current_match_rank > proposer_match_rank and proposer != rejector:
-                    print(f"{rejector} is now matched with {proposer} instead.")
-                    print()
+                    #print(f"{rejector} is now matched with {proposer} instead.")
+                    #print()
                     matches[rejector] = proposer
                     matches[proposer] = rejector
                     remaining_proposers.add(existing_match)
                     break
     return matches
 
-def is_unstable(matches):
-    potential_unstables = set()
-    for name in matches.keys():
-        partner = matches[name]
-        rank = maps[name][partner]
-        print(f"{name} is matched with {partner} at a rank of {rank}")
-        if rank > 1:
-            potential_unstables.add(name)
+def are_disjoint(a, b):
+    a_length = len(a)
+    a_as_set = set(a)
+    b_length = len(b)
+    b_as_set = set(b)
+    if a_length != len(a_as_set) or b_length != len(b_as_set):
+        return False
+    return a_as_set.isdisjoint(b_as_set)
 
-
-
+def contains_none(a, b):
+    return None in a or None in b
 
 def main():
     read_input()
     split_names()
     # print_data()
     matches = tinder()
-    if is_unstable(matches):
-        print("-")
-    else:
-        final_matches = list(map(lambda key: (key, matches.get(key)), proposers))
-        print()
+    final_proposers = []
+    final_rejectors = []
+    final_matches = list(map(lambda key: (key, matches.get(key)), proposers))
+    for (proposer, rejector) in final_matches:
+        final_proposers.append(proposer)
+        final_rejectors.append(rejector)
+
+    if are_disjoint(final_proposers, final_rejectors) and not contains_none(final_proposers, final_rejectors):
         for (proposer, rejector) in final_matches:
             print(f"{proposer} {rejector}")
+    else: print("-")
 
 
 if __name__ == "__main__":
