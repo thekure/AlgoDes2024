ccd1a1742acd40d745621fe4284e581c999b2c69
Laurits <laku@itu.dk>
2024-09-04 13:05:58 +0200

    minimum scalar should work but doesn't

--- KattisFSharp/nn_minimum_scalar_product.fs
+++ KattisFSharp/nn_minimum_scalar_product.fs
@@ -10,7 +10,6 @@ let readVector dim =
         match i with
         | i when i = dim -> acc |> List.rev
         | i ->
-            // read values as int64 to avoid 
             let value = scanner.NextInt()
             aux (i+1) (int64 value :: acc)
     aux 0 List.empty
@@ -26,26 +25,23 @@ let readInput =
             aux (i+1) ((v1,v2)::acc)
     aux 0 List.empty
     
-let calculateScalarProduct v1 v2 =
-    let rec aux v1 v2 acc =
-        match v1, v2 with
-        | [], [] -> int acc
-        | x1::xs1, x2::xs2 -> 
-            let product = x1 * x2
-            aux xs1 xs2 (acc + product)
-        | _ -> failwith "Vectors must be of the same length."
-    aux v1 v2 0L
-    
+let rec calculateScalarProduct v1 v2 acc =
+    match v1, v2 with
+    | [], [] -> int acc
+    | x1::xs1, x2::xs2 -> 
+        let product = x1 * x2
+        calculateScalarProduct xs1 xs2 (acc + product)
+    | _ -> failwith "Vectors must be of the same length."
 
 let handleCase (v1,v2) =
     let result1 =
         let smallestV1 = List.sort v1
         let largestV2 = List.sortDescending v2
-        calculateScalarProduct smallestV1 largestV2
+        calculateScalarProduct smallestV1 largestV2 0L
     let result2 =
         let largestV1 = List.sortDescending v1
         let smallestV2 = List.sort v2
-        calculateScalarProduct largestV1 smallestV2
+        calculateScalarProduct largestV1 smallestV2 0L
     if result1 <= result2 then result1 else result2
 
 let handleAllCases cases =
