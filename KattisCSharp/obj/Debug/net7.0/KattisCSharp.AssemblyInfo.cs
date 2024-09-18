9a06342ed102f6c3ff418ae560d23c11b7e2fbad
Kure <laku@itu.dk>
2024-09-18 17:45:41 +0200

    switch

--- KattisCSharp/obj/Debug/net7.0/KattisCSharp.AssemblyInfo.cs
+++ KattisCSharp/obj/Debug/net7.0/KattisCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KattisCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ea023432441695337798a0fd443bc5e11e5c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e49e0e2468b436ee5a8da72898441be73c6ae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("KattisCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KattisCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
